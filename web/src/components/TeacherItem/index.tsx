841c92402a61910c733e54e120c949bd1c311706
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2020-08-07 23:02:52 -0300

    [WEB] - add GiveClasses page and add the comunication with the api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,53 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
  return (
   <article className="teacher-item">
     <header>
-      <img src="https://avatars0.githubusercontent.com/u/17408674?s=460&u=351a0f8f7ce9e420cdd8e3b97fde3d0c274f6b43&v=4" alt="Matheus Lorenzeto"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Matheus Mendes Lorenzeto</strong>
-        <span>Química</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Entusiasta das melhores tecnologias de química avançada.
-      <br/><br/>
-      Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-    </p>
+    <p>{teacher.bio}</p>
 
     <footer>
       <p>
         Preço/hora 
-        <strong>R$ 80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="submit">
+      <a 
+        onClick={createNewConnection}
+        href={`https://wa.me/${teacher.whatsapp}`} 
+        target="_blank"       
+      >
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em contato
-      </button>
+      </a>
     </footer>            
   </article>
  ); 
